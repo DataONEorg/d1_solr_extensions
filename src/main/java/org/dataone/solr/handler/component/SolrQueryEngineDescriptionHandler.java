93d5cfc80b4f4ac9e4c2bee388965a1c487b116c
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-09-28 03:23:32 +0000

    updated to read solr version from solr package info. reads schema version from property file.

--- src/main/java/org/dataone/solr/handler/component/SolrQueryEngineDescriptionHandler.java
+++ src/main/java/org/dataone/solr/handler/component/SolrQueryEngineDescriptionHandler.java
@@ -1,9 +1,16 @@
 package org.dataone.solr.handler.component;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.common.util.XML;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.handler.admin.LukeRequestHandler;
 import org.apache.solr.request.SolrQueryRequest;
@@ -18,6 +25,8 @@ public class SolrQueryEngineDescriptionHandler extends LukeRequestHandler implem
     private String schemaVersion = "1.0";
     private String name = "solr";
     private String additionalInfo = null;
+    private static final String SCHEMA_PROPERTIES_PATH = "/etc/dataone/index/solr/schema.properties";
+    private static final String SCHEMA_VERSION_PROPERTY = "schema-version=";
 
     private Map<String, SchemaField> fieldMap = null;
 
@@ -25,6 +34,50 @@ public class SolrQueryEngineDescriptionHandler extends LukeRequestHandler implem
     public void inform(SolrCore core) {
         IndexSchema schema = core.getSchema();
         fieldMap = schema.getFields();
+        setSchemaVersionFromPropertiesFile();
+        setSolrVersion();
+    }
+
+    /**
+     * Based on org.apache.solr.handler.admin.SystemInfoHandler.getLuceneInfo()
+     */
+    private void setSolrVersion() {
+        String solrSpecVersion = "";
+
+        Package p = SolrCore.class.getPackage();
+        StringWriter tmp = new StringWriter();
+        solrSpecVersion = p.getSpecificationVersion();
+        if (null != solrSpecVersion) {
+            try {
+                XML.escapeCharData(solrSpecVersion, tmp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            solrSpecVersion = tmp.toString();
+        }
+        if (StringUtils.isNotBlank(solrSpecVersion)) {
+            this.solrVersion = solrSpecVersion;
+        }
+    }
+
+    private void setSchemaVersionFromPropertiesFile() {
+        File file = new File(SCHEMA_PROPERTIES_PATH);
+        if (file.exists()) {
+            try {
+                List lines = FileUtils.readLines(file, "UTF-8");
+                for (Object object : lines) {
+                    String line = (String) object;
+                    if (line.startsWith(SCHEMA_VERSION_PROPERTY)) {
+                        String version = StringUtils.substringAfter(line, SCHEMA_VERSION_PROPERTY);
+                        if (StringUtils.isNotBlank(version)) {
+                            this.schemaVersion = version;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
