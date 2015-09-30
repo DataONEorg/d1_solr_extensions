cdba4bd9eb41e79d83831cf2a7a096b85882d33e
Sky Roseboom <sroseboo@epscor.unm.edu>
2015-09-30 20:22:37 +0000

    creating extension of HttpSolrCall to provide original HttpServletRequest in SolrQueryRequest context.

--- src/main/java/org/dataone/solr/servlet/DataOneSolrDispatchFilter.java
+++ src/main/java/org/dataone/solr/servlet/DataOneSolrDispatchFilter.java
@@ -1,19 +1,28 @@
 package org.dataone.solr.servlet;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.solr.servlet.HttpSolrCall;
 import org.apache.solr.servlet.SolrDispatchFilter;
 import org.apache.solr.servlet.SolrRequestParsers;
-import org.dataone.solr.handler.component.SolrSearchHandler;
 
 public class DataOneSolrDispatchFilter extends SolrDispatchFilter {
 
-    private static Log logger = LogFactory.getLog(SolrSearchHandler.class);
+    private static Log logger = LogFactory.getLog(DataOneSolrDispatchFilter.class);
 
     public DataOneSolrDispatchFilter() {
         logger.debug("setting solr request parsers add request headers to context to true");
         SolrRequestParsers.DEFAULT.setAddRequestHeadersToContext(true);
         logger.debug("SolrRequestParsers.DEFAULT.getAddRequestHeadersToContext is: "
                 + SolrRequestParsers.DEFAULT.isAddRequestHeadersToContext());
+    }
+
+    @Override
+    protected HttpSolrCall getHttpSolrCall(HttpServletRequest request,
+            HttpServletResponse response, boolean retry) {
+        return new DataOneHttpSolrCall(this, cores, request, response, retry);
     }
 }
