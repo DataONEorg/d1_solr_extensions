ef63691044bc77e38039f93daf5d4328c4937a5a
Sky Roseboom <sroseboo@epscor.unm.edu>
2015-09-30 19:49:04 +0000

    add some logging to verify condition

--- src/main/java/org/dataone/solr/servlet/DataOneSolrDispatchFilter.java
+++ src/main/java/org/dataone/solr/servlet/DataOneSolrDispatchFilter.java
@@ -1,11 +1,19 @@
 package org.dataone.solr.servlet;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.solr.servlet.SolrDispatchFilter;
 import org.apache.solr.servlet.SolrRequestParsers;
+import org.dataone.solr.handler.component.SolrSearchHandler;
 
 public class DataOneSolrDispatchFilter extends SolrDispatchFilter {
 
+    private static Log logger = LogFactory.getLog(SolrSearchHandler.class);
+
     public DataOneSolrDispatchFilter() {
+        logger.debug("setting solr request parsers add request headers to context to true");
         SolrRequestParsers.DEFAULT.setAddRequestHeadersToContext(true);
+        logger.debug("SolrRequestParsers.DEFAULT.getAddRequestHeadersToContext is: "
+                + SolrRequestParsers.DEFAULT.isAddRequestHeadersToContext());
     }
 }
