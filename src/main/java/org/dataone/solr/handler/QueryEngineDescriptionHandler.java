da22e010c832b0083822c56eca038e2eea429c7f
Jing Tao <tao@nceas.ucsb.edu>
2026-03-04 17:36:14 -0800

    Upgrade solr library from 5 to 9.10.1 and make it compilable.

--- src/main/java/org/dataone/solr/handler/QueryEngineDescriptionHandler.java
+++ src/main/java/org/dataone/solr/handler/QueryEngineDescriptionHandler.java
@@ -1,24 +1,3 @@
-/**
- * This work was created by participants in the DataONE project, and is
- * jointly copyrighted by participating institutions in DataONE. For 
- * more information on DataONE, see our web site at http://dataone.org.
- *
- *   Copyright ${year}
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
- * limitations under the License.
- * 
- * $Id$
- */
 package org.dataone.solr.handler;
 
 import java.io.File;
@@ -191,17 +170,14 @@ public abstract class QueryEngineDescriptionHandler extends RequestHandlerBase i
         return "Solr Query Engine Description Handler.";
     }
 
-    @Override
     public String getVersion() {
         return "Version 1.0";
     }
 
-    @Override
     public String getSource() {
         return "https://repository.dataone.org/";
     }
 
-    @Override
     public URL[] getDocs() {
         try {
             return new URL[] { new URL("http://mule1.dataone.org/ArchitectureDocs-current/") };
